9bb67c2100800ac8d49cf63a41cdf0090f4f3db8
michisthr <michlstoehr19@icloud.com>
2025-06-24 21:36:34 +0200

    use tabs

--- app/(tabs)/calendar/_layout.tsx
+++ app/(tabs)/calendar/_layout.tsx
@@ -1,36 +1,40 @@
 import { Stack } from "expo-router";
 import React from "react";
 import { StyleSheet } from "react-native";
-import Event from "./event"
-
 
 const CalendarLayout = () => {
-  return (
-    <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false, contentStyle: { backgroundColor: "white" } }} />
-      <Stack.Screen
-        name='add_event'
-        options={{
-          headerShown: false,
-          presentation: "formSheet",
-        }}
-      />
-      <Stack.Screen
-        name='event'
-        options={{
-          headerShown: false,
-          presentation: "modal",
-        }}
-      />
-      <Stack.Screen
-        name='edit_event'
-        options={{
-          headerShown: false,
-          presentation: "modal",
-        }}
-      />
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Stack.Screen
+                name='index'
+                options={{
+                    headerShown: false,
+                    contentStyle: { backgroundColor: "white" },
+                }}
+            />
+            <Stack.Screen
+                name='add_event'
+                options={{
+                    headerShown: false,
+                    presentation: "formSheet",
+                }}
+            />
+            <Stack.Screen
+                name='category'
+                options={{
+                    headerShown: false,
+                    presentation: "formSheet",
+                }}
+            />
+            <Stack.Screen
+                name='add_category'
+                options={{
+                    headerShown: false,
+                    presentation: "formSheet",
+                }}
+            />
+        </Stack>
+    );
 };
 
 export default CalendarLayout;
--- app/(tabs)/calendar/edit_event.tsx
+++ app/(tabs)/calendar/edit_event.tsx
@@ -1,35 +1,11 @@
-import CozyInput from "@/components/cozy_input";
 import { useNavigation } from "expo-router";
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { getKalender } from "../../../lib/appwrite/dbKalender"; //für db
+import { StyleSheet, View } from "react-native";
 
 const EditEvent = () => {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 30,
-          fontWeight: "bold",
-          textAlign: "center",
-          marginBottom: 20,
-        }}>
-        Add new Event
-      </Text>
-      {/* <Button onPress={() => navigation.goBack()} title='Dismiss' /> */}
-      <CozyInput placeholder='Name' placeholderTextColor={"black"} />
-      <CozyInput placeholder='Creator' placeholderTextColor={"black"} />
-      <CozyInput placeholder='Color' placeholderTextColor={"black"} />
-      <CozyInput placeholder='Date' placeholderTextColor={"black"} />
-      <CozyInput placeholder='Description' placeholderTextColor={"black"} />
-      <Button
-        title='Create'
-         onPress={async () => {
-         const inhalt = await getKalender();
-        //  console.log("Kalenderinhalt: ", inhalt);
-        }}
-      />
     </View>
   );
 };
@@ -43,9 +19,5 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: "center",
     marginHorizontal: 30,
-  },
-
-  button: {
-    borderRadius: 10,
-  },
+  }
 });
