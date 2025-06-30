081f20e713f543760c3919c87e051cc565129338
Sophia Schade <sophia.schade@stud.hs-kempten.de>
2025-06-30 13:54:45 +0200

    fixed error message

--- app/(tabs)/todo/newtodo.tsx
+++ app/(tabs)/todo/newtodo.tsx
@@ -1,4 +1,4 @@
-import { Badge, BadgeText, Box, Button, ButtonText, HStack, VStack } from "@gluestack-ui/themed";
+import { Box, Button, ButtonText, HStack, VStack } from "@gluestack-ui/themed";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { router } from "expo-router";
 import { CalendarDays } from "lucide-react-native"; // oder deine Icon-Bibliothek
@@ -7,7 +7,7 @@ import { Platform, SafeAreaView, Text, TextInput, TouchableOpacity, View } from 
 import DropDownPicker from "react-native-dropdown-picker";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { addTodo } from "../../../lib/appwrite/dbTodo"; //für db
-import styles, { containerWidth } from "./styles";
+import styles from "./styles";
 
 const isWeb = Platform.OS === 'web';
 
@@ -220,7 +220,7 @@ export default function NewToDo() {
   const [selectedPerson, setSelectedPerson] = useState('');
   const [selectedRepeat, setSelectedRepeat] = useState('');
   const [selectedLabel, setSelectedLabel] = useState('');
-  const [successMessage, setSuccessMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState('');
   const [todoName, setTodoName] = useState('');
   const [date, setDate] = useState<Date | null>(null);
   const [show, setShow] = useState(false);
@@ -264,7 +264,8 @@ export default function NewToDo() {
   }
 
   return (
-    <SafeAreaView style={[styles.container_box, { paddingBottom: insets.bottom + 32 }]}>      <Text style={styles.heading}>Add new To Do</Text>
+    <SafeAreaView style={[styles.container_box, { paddingBottom: insets.bottom + 32 }]}>      
+    <Text style={styles.heading}>Add new To Do</Text>
       {errorMessage !== "" && (
         <View style={{ position: "absolute", alignItems: "center", zIndex: 2000, marginTop: "20%", width: containerWidth }}>
           <Badge style={styles.badgeErrorMessage}><BadgeText style={styles.badgeErrorMessageText}>{errorMessage}</BadgeText></Badge>
