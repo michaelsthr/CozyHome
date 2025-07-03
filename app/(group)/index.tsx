e0f7b727ff7046cf5cd6b64ae062cb037d816148
matthias.steger <matthias.stgr@gmail.com>
2025-07-03 17:46:06 +0200

    fixed user chang in group selection

--- app/(group)/index.tsx
+++ app/(group)/index.tsx
@@ -6,102 +6,102 @@ import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from "react-n
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-    const router = useRouter();
-    const navigation = useNavigation();
-    const { group, user, setUser, setGroup } = useSession();
+  const router = useRouter();
+  const navigation = useNavigation();
+  const { group, user, setUser, setGroup } = useSession();
 
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false,
-        });
-    }, [navigation]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
 
-    function goBack() {
-        setUser(null);
-        router.back();
-    }
+  function otherUser() {
+    setUser(null);
+    router.push("/(auth)");
+  }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            {/* Header */}
-            <View style={{ alignItems: "center" }}>
-                <Text
-                    style={{
-                        fontSize: 40,
-                        fontWeight: "800",
-                        color: "#7749f8",
-                        letterSpacing: -1,
-                        lineHeight: 40,
-                        paddingTop: 10,
-                    }}>
-                    Group Selection
-                </Text>
-                <Image
-                    source={require("@/assets/images/groups_page.png")}
-                    style={{
-                        width: 320,
-                        height: 320,
-                    }}
-                    resizeMode='contain'
-                />
-            </View>
-            <View style={{ width: "100%", paddingHorizontal: 32 }}>
-                <Text style={[fontStyles.lighter, { paddingVertical: 20, textAlign: "center" }]}>
-                    Do you want to join an existing group or create a new one?
-                </Text>
-                <TouchableOpacity
-                    style={{
-                        backgroundColor: "#7749f8",
-                        padding: 20,
-                        borderRadius: 12,
-                        alignItems: "center",
-                        marginBottom: 20,
-                        height: 100,
-                        justifyContent: "center",
-                    }}
-                    onPress={() => router.push("/(group)/existingGroup")}>
-                    <Text style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}>
-                        {" "}
-                        Join Existing Group
-                    </Text>
-                </TouchableOpacity>
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Header */}
+      <View style={{ alignItems: "center" }}>
+        <Text
+          style={{
+            fontSize: 40,
+            fontWeight: "800",
+            color: "#7749f8",
+            letterSpacing: -1,
+            lineHeight: 40,
+            paddingTop: 10,
+          }}>
+          Group Selection
+        </Text>
+        <Image
+          source={require("@/assets/images/groups_page.png")}
+          style={{
+            width: 320,
+            height: 320,
+          }}
+          resizeMode='contain'
+        />
+      </View>
+      <View style={{ width: "100%", paddingHorizontal: 32 }}>
+        <Text style={[fontStyles.lighter, { paddingVertical: 20, textAlign: "center" }]}>
+          Do you want to join an existing group or create a new one?
+        </Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#7749f8",
+            padding: 20,
+            borderRadius: 12,
+            alignItems: "center",
+            marginBottom: 20,
+            height: 100,
+            justifyContent: "center",
+          }}
+          onPress={() => router.push("/(group)/existingGroup")}>
+          <Text style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}>
+            {" "}
+            Join Existing Group
+          </Text>
+        </TouchableOpacity>
 
-                <TouchableOpacity
-                    style={{
-                        backgroundColor: "#7749f8",
-                        padding: 20,
-                        borderRadius: 12,
-                        alignItems: "center",
-                        height: 100,
-                        justifyContent: "center",
-                    }}
-                    onPress={() => router.push("/(group)/newGroup")}>
-                    <Text style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}>
-                        New Group
-                    </Text>
-                </TouchableOpacity>
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: "100%",
-                        paddingTop: 20,
-                    }}>
-                    <Button title="Go Back" color="#7749f8" onPress={goBack}/>
-                </View>
-            </View>
-        </SafeAreaView>
-    );
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#7749f8",
+            padding: 20,
+            borderRadius: 12,
+            alignItems: "center",
+            height: 100,
+            justifyContent: "center",
+          }}
+          onPress={() => router.push("/(group)/newGroup")}>
+          <Text style={{ color: "#fff", fontSize: 16, fontWeight: "600" }}>
+            New Group
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            paddingTop: 20,
+          }}>
+          <Button title="Other user" color="#7749f8" onPress={otherUser} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingVertical: 20,
-        backgroundColor: "white",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 20,
+    backgroundColor: "white",
+  },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -95,7 +95,7 @@ export default function HomePage() {
       setLoading(true);
 
       setUser(null);
-      router.push
+      router.push("/(auth)");
 
     } catch (error) {
       console.error("Error log out:", error);
