748a6c699039f42460b68b74711e0c5086319677
michisthr <michlstoehr19@icloud.com>
2025-07-06 22:05:18 +0200

    remove loading circle

--- app/(tabs)/fridge/fridge_home.tsx
+++ app/(tabs)/fridge/fridge_home.tsx
@@ -135,13 +135,13 @@ export default function Fridge() {
     }
   };
 
-  if (loading) {
-    return (
-      <SafeAreaView style={[styles.container, { flex: 1, justifyContent: 'center', alignItems: 'center' }]}>
-        <ActivityIndicator size="large" color="#8B5CF6" />
-      </SafeAreaView>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <SafeAreaView style={[styles.container, { flex: 1, justifyContent: 'center', alignItems: 'center' }]}>
+  //       <ActivityIndicator size="large" color="#8B5CF6" />
+  //     </SafeAreaView>
+  //   );
+  // } Sorry Julian 
 
   return (
     <SafeAreaView style={styles.container}>
